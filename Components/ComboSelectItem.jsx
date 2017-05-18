709b2e8c96a37e15da0936ae6696555095d7905c
Lana Petkovic <lpetkovic@gogoair.com>
2017-05-18 13:33:30 +0200

    Add scrollMaxHeight prop; Use ids or indexes as keys

--- Components/ComboSelectItem.jsx
+++ Components/ComboSelectItem.jsx
@@ -2,86 +2,87 @@ import React, { Component } from 'react';
 
 export default class ComboSelectItem extends Component {
 
-    _generateInput() {
-        let input;
+	_generateInput() {
+		let input;
 
-        if (this.props.type == 'select') {
+		if (this.props.type == 'select') {
 
-            if (this.props.selected) {
+			if (this.props.selected) {
 
-                if (this.props.iconSelectActive === true || this.props.iconSelectActive === undefined || this.props.iconSelectActive === null) {
-                    input = <i className="fa fa-check-circle"></i>;
-                } else if (this.props.iconSelectActive === false || this.props.iconSelectActive === 'off') {
-                    input = '';
-                } else {
-                    input = <i className={this.props.iconSelectActive}></i>;
-                }
+				if (this.props.iconSelectActive === true || this.props.iconSelectActive === undefined || this.props.iconSelectActive === null) {
+					input = <i className="fa fa-check-circle"></i>;
+				} else if (this.props.iconSelectActive === false || this.props.iconSelectActive === 'off') {
+					input = '';
+				} else {
+					input = <i className={this.props.iconSelectActive}></i>;
+				}
 
-            } else {
+			} else {
 
-                if (this.props.iconSelectInactive === true || this.props.iconSelectActive === undefined || this.props.iconSelectActive === null) {
-                    input = <i className="fa fa-circle-o"></i>;
-                } else if (this.props.iconSelectInactive === false || this.props.iconSelectInactive === 'off') {
-                    input = '';
-                } else {
-                    input = <i className={this.props.iconSelectInactive}></i>;
-                }
+				if (this.props.iconSelectInactive === true || this.props.iconSelectActive === undefined || this.props.iconSelectActive === null) {
+					input = <i className="fa fa-circle-o"></i>;
+				} else if (this.props.iconSelectInactive === false || this.props.iconSelectInactive === 'off') {
+					input = '';
+				} else {
+					input = <i className={this.props.iconSelectInactive}></i>;
+				}
 
-            }
+			}
 
-        } else {
+		} else {
 
-            if (this.props.selected) {
+			if (this.props.selected) {
 
-                if (this.props.iconSelectActive === true || this.props.iconSelectActive === undefined || this.props.iconSelectActive === null) {
-                    input = <i className="fa fa-check-square"></i>;
-                } else if (this.props.iconSelectActive === false || this.props.iconSelectActive === 'off') {
-                    input = '';
-                } else {
-                    input = <i className={this.props.iconSelectActive}></i>;
-                }
+				if (this.props.iconSelectActive === true || this.props.iconSelectActive === undefined || this.props.iconSelectActive === null) {
+					input = <i className="fa fa-check-square"></i>;
+				} else if (this.props.iconSelectActive === false || this.props.iconSelectActive === 'off') {
+					input = '';
+				} else {
+					input = <i className={this.props.iconSelectActive}></i>;
+				}
 
-            } else {
+			} else {
 
-                if (this.props.iconSelectInactive === true || this.props.iconSelectActive === undefined || this.props.iconSelectActive === null) {
-                    input = <i className="fa fa-square-o"></i>;
-                } else if (this.props.iconSelectInactive === false || this.props.iconSelectInactive === 'off') {
-                    input = '';
-                } else {
-                    input = <i className={this.props.iconSelectInactive}></i>;
-                }
+				if (this.props.iconSelectInactive === true || this.props.iconSelectActive === undefined || this.props.iconSelectActive === null) {
+					input = <i className="fa fa-square-o"></i>;
+				} else if (this.props.iconSelectInactive === false || this.props.iconSelectInactive === 'off') {
+					input = '';
+				} else {
+					input = <i className={this.props.iconSelectInactive}></i>;
+				}
 
-            }
+			}
 
-        }
+		}
 
-        return input;
-    }
+		return input;
+	}
 
-    render() {
-        let input = this._generateInput();
+	render() {
+		let input = this._generateInput();
+		let id = (this.props.item && this.props.item.value && this.props.item.value.id) ? this.props.item.value.id : null;
 
-        return (
-            <div
-                key={Math.floor(Math.random() * 1000001111)}
-                className={'combo-select-item' + ((this.props.focused) ? ' active' : '') + ((this.props.selected) ? ' selected' : '') + (input == '' ? ' no-icon' : '')}
-                onClick={() => this.props.selectItem(this.props.item)}
-                onMouseEnter={() => this.props.focusItem(this.props.index)}>
-                {input}
-                {this.props.item.text}
-            </div>
-        );
-    }
+		return (
+			<div
+				key={id ? id : this.props.index}
+				className={'combo-select-item' + ((this.props.focused) ? ' active' : '') + ((this.props.selected) ? ' selected' : '') + (input == '' ? ' no-icon' : '')}
+				onClick={() => this.props.selectItem(this.props.item)}
+				onMouseEnter={() => this.props.focusItem(this.props.index)}>
+				{input}
+				{this.props.item.text}
+			</div>
+		);
+	}
 }
 
 ComboSelectItem.propTypes = {
-    item: React.PropTypes.any,
-    index: React.PropTypes.number,
-    focused: React.PropTypes.bool,
-    selected: React.PropTypes.bool,
-    type: React.PropTypes.string,
-    selectItem: React.PropTypes.func,
-    focusItem: React.PropTypes.func,
-    iconSelectActive: React.PropTypes.any,
-    iconSelectInactive: React.PropTypes.any
+	item: React.PropTypes.any,
+	index: React.PropTypes.number,
+	focused: React.PropTypes.bool,
+	selected: React.PropTypes.bool,
+	type: React.PropTypes.string,
+	selectItem: React.PropTypes.func,
+	focusItem: React.PropTypes.func,
+	iconSelectActive: React.PropTypes.any,
+	iconSelectInactive: React.PropTypes.any
 };
