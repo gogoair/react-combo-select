cb26a68a6b27d6706582f180f724a44e790fbfb7
Miloš Rašić <mrasic@gogoair.com>
2017-11-15 15:01:49 +0100

    added immutable as dev dep because chai-immutable is bad, added enzyme configuration with adapter to test.js

--- test/test.js
+++ test/test.js
@@ -1,6 +1,10 @@
 import jsdom from 'jsdom';
 import chai from 'chai';
 import chaiImmutable from 'chai-immutable';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
 
 const doc = jsdom.jsdom('<!doctype html><html><body></body></html>');
 const win = doc.defaultView;
