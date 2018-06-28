5519aa4839dfd82853a83ae6301c30f4a4d15ece
Darko Tasevski <c.dtasevski@gogoair.com>
2018-06-28 13:04:09 +0200

    Update demo data

--- test/fixtures.js
+++ test/fixtures.js
@@ -122,3 +122,134 @@ export const complexAlphanumData = [
 	{ text: 'Xiph Xlater 5000' },
 	{ text: 'Xiph Xlater 58' },
 ];
+
+export const demoData = {
+	data: [
+		{ text: '1air-JA007D', win: 'win-111', value: 'JA007D', number: 0 },
+		{ text: '1air-JA008D', win: 'win-222', value: 'JA008D', number: 0 },
+		{ text: '1air-JA009D', win: 'win-333', value: 'JA009D', number: 1 },
+		{ text: '111air-JA107D', win: 'win-444', value: 'JA010D', number: 1 },
+		{ text: '111air-JA107R', win: 'win-555', value: 'JA107R', number: 1 },
+		{ text: '11air-JA008D1', win: 'win-2221', value: 'JA0081D', number: 0 },
+		{ text: '11air-JA009D2', win: 'win-3332', value: 'JA009D2', number: 1 },
+		{ text: '11air-JA107D3', win: 'win-4443', value: 'JA010D3', number: 1 },
+		{ text: '11air-JA107R4', win: 'win-5554', value: 'JA107R4', number: 1 },
+		{ text: '11air-JA008D5', win: 'win-2225', value: 'JA008D5', number: 0 },
+		{ text: 'air-JA009D8', win: 'win-3336', value: 'JA009D6', number: 1 },
+		{ text: 'air-JA107D6', win: 'win-4447', value: 'JA010D7', number: 1 },
+		{ text: 'air-JA107R7', win: 'win-5558', value: 'JA107R8', number: 1 },
+		{ text: 'air-JA107Y9', win: 'win-6669', value: 'JA107Y9', number: 1 },
+	],
+	groups: [
+		{
+			groupName: 'Group 1',
+			options: [
+				{
+					text: 'AGP',
+					win: 'AGP',
+					value: 'AGP',
+					number: 1,
+				},
+				{
+					text: 'COM',
+					win: 'COM',
+					value: 'COM',
+					number: 1,
+				},
+				{
+					text: 'XSS',
+					win: 'XSS',
+					value: 'XSS',
+					number: 1,
+				},
+				{
+					text: 'ZZT',
+					win: 'ZZT',
+					value: 'ZZT',
+					number: 1,
+				},
+				{
+					text: 'JAL',
+					win: 'JAL',
+					value: 'JAL',
+					number: 1,
+				},
+				{
+					text: 'WOR',
+					win: 'WOR',
+					value: 'WOR',
+					number: 1,
+				},
+			],
+		},
+		{
+			groupName: 'Group 2',
+			options: [
+				{
+					text: 'virtual',
+					win: 'virtual',
+					value: 'virtual',
+					number: 1,
+				},
+				{
+					text: 'wireless',
+					win: 'wireless',
+					value: 'wireless',
+					number: 1,
+				},
+				{
+					text: 'primary',
+					win: 'primary',
+					value: 'primary',
+					number: 1,
+				},
+			],
+		},
+		{
+			groupName: 'Group 3',
+			options: [
+				{
+					text: 'Champlin LLC',
+					win: 'Champlin LLC',
+					value: 'Champlin LLC',
+					number: 1,
+				},
+				{
+					text: 'Erdman Inc',
+					win: 'Erdman Inc',
+					value: 'Erdman Inc',
+					number: 1,
+				},
+				{
+					text: 'Bergnaum and Sons',
+					win: 'Bergnaum and Sons',
+					value: 'Bergnaum and Sons',
+					number: 1,
+				},
+				{
+					text: 'Yundt LLC',
+					win: 'Yundt LLC',
+					value: 'Yundt LLC',
+					number: 1,
+				},
+			],
+		},
+		{
+			groupName: 'Group 4',
+			options: [
+				{
+					text: 'T-Mobile',
+					win: 'T-Mobile',
+					value: 'T-Mobile',
+					number: 1,
+				},
+				{
+					text: 'Startek',
+					win: 'Startek',
+					value: 'Startek',
+					number: 1,
+				},
+			],
+		},
+	],
+};
