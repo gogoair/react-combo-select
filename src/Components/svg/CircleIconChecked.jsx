67b91c787b942eca65ac5eb339299373c0a2c40e
Quang Tran <quangta93@gmail.com>
2019-08-19 11:45:19 -0500

    fix console error by letting event bubble to helper function

--- src/Components/svg/CircleIconChecked.jsx
+++ src/Components/svg/CircleIconChecked.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactSvgIcon, { enabledColor } from './ReactSvgIcon/index';
 
-const DropDownIcon = ({ handleClick }) => (
-	<ReactSvgIcon handleClick={handleClick} className="SvgIconCircleIconChecked">
+const DropDownIcon = (props) => (
+	<ReactSvgIcon {...props} className="SvgIconCircleIconChecked">
 		<path
 			fill={enabledColor}
 			d="M256,8C119,8,8,119,8,256s111,248,248,248s248-111,248-248S393,8,256,8z M396.1,203.2L223.5,374.4c-4.7,4.7-12.3,4.6-17-0.1l-90.8-91.5c-4.7-4.7-4.6-12.3,0.1-17l22.7-22.5c4.7-4.7,12.3-4.6,17,0.1l59.8,60.3l141.4-140.2c4.7-4.7,12.3-4.6,17,0.1l22.5,22.7C400.9,191,400.8,198.6,396.1,203.2z"
--- src/Components/svg/ReactSvgIcon/index.jsx
+++ src/Components/svg/ReactSvgIcon/index.jsx
@@ -6,18 +6,12 @@ export const disabledColor = '#d1d3d4';
 export const enabledColor = '#45b3e3';
 
 const ReactSvgIcon = (props = {}) => {
-	const { width, height, children, handleClick, ...restProps } = props;
-
-	const onClick = e => {
-		e.stopPropagation();
-		return handleClick();
-	};
+	const { width, height, children, ...restProps } = props;
 
 	return (
 		<svg
 			xmlns="http://www.w3.org/2000/svg"
 			xmlnsXlink="http://www.w3.org/1999/xlink"
-			onClick={onClick}
 			viewBox="0 0 512 512"
 			{...restProps}
 			width={(width && formatLength(width)) || '1em'}
